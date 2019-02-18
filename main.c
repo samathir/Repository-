d03d315a56fc4356beee4b3f87a4d25ff8e02b82
samathir <43017727+samathir@users.noreply.github.com>
2019-02-18 10:29:05 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "life.h"
+
+/* Be sure to read life.h and the other given source files to understand
+ * what they provide and how they fit together.  You don't have to
+ * understand all of the code, but you should understand how to call
+ * parse_life() and clearterm().
+ */
+
+/* This is where your program will start.  You should make sure that it
+ * is capable of accepting either one or two arguments; the first
+ * argument (which is required) is a starting state in one of the Life
+ * formats supported by parse_life(), and the second (which is optional)
+ * is a number of generations to run before printing output and stopping.
+ *
+ * The autograder will always call your program with two arguments.  The
+ * one-argument format (as described in the handout) is for your own
+ * benefit!
+ */
+int main(int argc, char *argv[])
+{
+    return 0;
+}
